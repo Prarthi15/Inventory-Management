7b601a1873e57e29fcdf3eb61efb1dea9fde5a1e
Prarthi15 <147303373+Prarthi15@users.noreply.github.com>
2024-08-07 17:35:56 +0530

    updated code

---
+++ lib/login_page.dart
@@ -0,0 +1,237 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 800) {
+            return Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.white,
+                    padding: const EdgeInsets.all(40.0),
+                    child: const LoginForm(),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: const Color.fromRGBO(37, 128, 69, 1),
+                    child: const Center(
+                      child: Text(
+                        "Katyayani",
+                        style: TextStyle(
+                          fontSize: 60,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: LoginForm(),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 300,
+                    color: const Color.fromRGBO(37, 128, 69, 1),
+                    child: const Center(
+                      child: Text(
+                        "Katyayani",
+                        style: TextStyle(
+                          fontSize: 60,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool _rememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Katyayani Organics",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(37, 128, 69, 1),
+          ),
+        ),
+        const SizedBox(height: 20),
+        const Text(
+          "Log in to your Account",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 10),
+        const Text("Welcome back! Select method to log in:"),
+        const SizedBox(height: 20),
+        Row(
+          children: [
+            Expanded(
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.google,
+                  color: Color.fromRGBO(66, 133, 244, 1),
+                  size: 18,
+                ),
+                label: const Text("Google"),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.white,
+                  shadowColor: Colors.grey,
+                  elevation: 5,
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.facebook,
+                  color: Color.fromRGBO(24, 119, 242, 1),
+                  size: 18,
+                ),
+                label: const Text("Facebook"),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.white,
+                  shadowColor: Colors.grey,
+                  elevation: 5,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        const Row(
+          children: [
+            Expanded(child: Divider(thickness: 1, color: Colors.grey)),
+            SizedBox(width: 10),
+            Text("or continue with email"),
+            SizedBox(width: 10),
+            Expanded(child: Divider(thickness: 1, color: Colors.grey)),
+          ],
+        ),
+        const SizedBox(height: 10),
+        const TextField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.mail),
+            labelText: "Email",
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(height: 10),
+        const TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.lock),
+            labelText: "Password",
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Checkbox(
+                  value: _rememberMe,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _rememberMe = value!;
+                    });
+                  },
+                  activeColor: const Color.fromRGBO(37, 128, 69, 1),
+                ),
+                const Text("Remember me"),
+              ],
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Forgot Password?",
+                style: TextStyle(color: Color.fromRGBO(37, 128, 69, 1)),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromRGBO(37, 128, 69, 1),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+            ),
+            child: const Text("Log in"),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Don't have an account? ",
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Create an account",
+                  style: TextStyle(color: Color.fromRGBO(37, 128, 69, 1)),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
